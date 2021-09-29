bba21250541ede5168c4aae02cecc6604f22f3df
Imme <andeser@protonmail.com>
2021-09-29 17:12:41 +0200

    Something idk

--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,9 @@ const uri = process.env.DB;
 module.exports = function(app) {
 
     mongoose.connect(process.env.DB);
-    const Message = mongoose.model('Message', {
+    const MessageSchema = mongoose.Schema({
         text: String,
+        board: String,
         created_on: Date,
         bumped_on: Date,
         reported: Boolean,
@@ -21,34 +22,49 @@ module.exports = function(app) {
         }]
     });
 
+    const Message = mongoose.model('Messageboard', MessageSchema);
+
+
     app.route('/api/threads/:board')
         .post((req, res) => {
-            const {text, delete_password, thread_id} = req.body;
-            if (thread_id){
-                Message.findByIdAndUpdate(thread_id, (err, doc) => {
-                    if (err) throw err;
-                    doc.bumped_on = new Date();
-                    doc.replies.push({
-                        _id: new mongoose.Types.ObjectId(),
-                        text: text,
-                        created_on: new Date(),
-                        delete_password: delete_password,
-                        reported: false
-                    });
-                });
-            }
-            else{
-                const newMessage = new Message({
-                    text: text,
-                    created_on: new Date(),
-                    bumped_on: new Date(),
-                    reported: false,
-                    delete_password: delete_password
-                });
-                newMessage.save();
-            }
+            const {
+                board,
+                text,
+                delete_password
+            } = req.body;
+
+            const newMessage = new Message({
+                text: text,
+                board: board,
+                created_on: new Date(),
+                bumped_on: new Date(),
+                reported: false,
+                delete_password: delete_password
+            });
+            newMessage.save();
+            res.redirect('/b/' + board);
         });
 
-    app.route('/api/replies/:board');
+    app.route('/api/replies/:board')
+      .post((req, res) => {
+            const {
+                board,
+                text,
+                delete_password,
+                thread_id
+            } = req.body;
 
+            Message.findByIdAndUpdate(thread_id, (err, doc) => {
+                if (err) throw err;
+                doc.bumped_on = new Date();
+                doc.replies.push({
+                    _id: new mongoose.Types.ObjectId(),
+                    text: text,
+                    created_on: new Date(),
+                    delete_password: delete_password,
+                    reported: false
+                });
+                doc.save();
+            });
+      });
 };
